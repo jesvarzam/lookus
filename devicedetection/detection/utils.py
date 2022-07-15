1556aa9ecd7eeef0d5eb68f71d79bc213050e79a
jesvarzam <jvargaszambrana0@gmail.com>
2022-07-15 19:22:44 +0200

    Added relation device-user

---
+++ devicedetection/detection/utils.py
@@ -0,0 +1,7 @@
+import re, validators
+
+def checkSingleFormat(device):
+
+    if re.search(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", device) or validators.url(device):
+        return True
+    return False
